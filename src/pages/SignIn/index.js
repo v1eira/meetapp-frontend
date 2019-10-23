8354472e6de2fc2d25ab3072e63f9c1580f960cf
v1eira <ewersonv@gmail.com>
2019-10-23 19:37:52 -0300

    add configuração do store (redux)

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="MeetApp" />
 
-      <form>
-        <input type="email" placeholder="Digite seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Digite seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Entrar</button>
         <Link to="/register">Criar conta grátis</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -34,6 +34,13 @@ export const Content = styled.div`
       }
     }
 
+    span {
+      color: #f94d6a;
+      align-self: flex-start;
+      margin: 0 0 10px 2px;
+      font-weight: bold;
+    }
+
     button {
       margin: 5px 0 0;
       height: 44px;
