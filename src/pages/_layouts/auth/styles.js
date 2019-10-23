29b6a030ad71a91c4605a3030f6e0a295ca980ee
v1eira <ewersonv@gmail.com>
2019-10-23 10:13:48 -0300

    add root import

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #22202c, #402845);
+  background: linear-gradient(#22202c, #402845);
 `;
